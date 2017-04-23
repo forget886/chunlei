9281d39a982a87ad96ba8944d594b9abed2d1ac8
zhanghui <zhanghui@souche.com>
2017-04-23 22:41:45 +0800

    加入 mybatis

--- src/main/java/com/wuxi/dao/BaseDao.java
+++ src/main/java/com/wuxi/dao/BaseDao.java
@@ -1,10 +1,13 @@
 package com.wuxi.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 public class BaseDao {
 
+//	@Autowired
+//	protected JdbcTemplate jdbcTemplate;
+	
 	@Autowired
-	protected JdbcTemplate jdbcTemplate;
+	protected SqlSessionTemplate sqlSession;
 }
--- src/main/java/com/wuxi/dao/UserDao.java
+++ src/main/java/com/wuxi/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.wuxi.dao;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -13,16 +14,22 @@ import com.wuxi.bean.vo.User;
 public class UserDao extends BaseDao{
 
 	
+//	public List<User> queryByName(String name){
+//		List<User> userList = new ArrayList<>();
+//		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from user where name = ?",new Object[]{name});
+//		Iterator<Map<String, Object>> it = list.iterator();  
+//		while(it.hasNext()) {  
+//		   Map userMap = (Map) it.next();  
+//		   User user = new User();
+//		   user.setName(userMap.get("name").toString());
+//		   userList.add(user);
+//		}  
+//		return userList;
+//	}
+	
 	public List<User> queryByName(String name){
-		List<User> userList = new ArrayList<>();
-		List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from user where name = ?",new Object[]{name});
-		Iterator<Map<String, Object>> it = list.iterator();  
-		while(it.hasNext()) {  
-		   Map userMap = (Map) it.next();  
-		   User user = new User();
-		   user.setName(userMap.get("name").toString());
-		   userList.add(user);
-		}  
-		return userList;
+		Map<String, Object> params = new HashMap<String,Object>();
+		params.put("name", name);
+		return this.sqlSession.selectList("user.getUser", params);
 	}
 }
