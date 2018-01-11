84bf4377c0a57cbc1fef9c51c07c873b63da5ce1
zhanghui <zhanghui@souche.com>
2018-01-11 23:00:55 +0800

    thread yield join

--- src/test/java/com/wuxi/service/ThreadTest.java
+++ src/test/java/com/wuxi/service/ThreadTest.java
@@ -1,164 +1,137 @@
 package com.wuxi.service;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
-import com.wuxi.service.CommonTest.SleepInterrupt;
-
 public class ThreadTest {
 
-	public static void main(String[] args) {
-		BoundedBuffer buffer = new BoundedBuffer();
-		ExecutorService executor = Executors.newFixedThreadPool(10);
-		executor.execute(new ReadThread("读1",buffer));
-		executor.execute(new ReadThread("读2",buffer));
-		executor.execute(new ReadThread("读3",buffer));
-		executor.execute(new ReadThread("读4",buffer));
-		executor.execute(new ReadThread("读5",buffer));
-		executor.execute(new WriteThread("写1",buffer));
-		executor.execute(new WriteThread("写2",buffer));
-		executor.execute(new WriteThread("写3",buffer));
-		executor.execute(new WriteThread("写4",buffer));
-		executor.execute(new WriteThread("写5",buffer));
+	@Test
+	public void create(){
+		ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 45, TimeUnit.SECONDS, new PriorityBlockingQueue<>(10));
+	}
+	
+	
+	@Test
+	public void yieldTest(){
+		Producer producer = new Producer();
+		Consumer consumer = new Consumer();
+		producer.setPriority(Thread.MIN_PRIORITY);
+		consumer.setPriority(Thread.MAX_PRIORITY);
+		producer.start();
+		consumer.start();
+	}
+	
+	
+	class Producer extends Thread{
+		
+		public void run(){
+			for(int i=0; i<5; i++){
+				System.out.println("condruce " + i);
+				//cpu让给别人
+				Thread.yield();
+			}
+		}
+	}
+
+	class Consumer extends Thread{
+		
+		public void run(){
+			for(int i=0; i<5; i++){
+				System.out.println("comsume " + i);
+				//cpu让给别人
+				Thread.yield();
+			}
+		}
+	}
+	
+	@Test
+	public void joinTest(){
+		Thread t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("t1 start..");
+				System.out.println("t1 sleep 2s...");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		t1.start();
+		try {
+			//t1执行完才能继续执行主线程
+			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("主线程执行...");
+	}
+
+	static volatile int i = 0 ;
+	
+	public  static void increase(){
+		synchronized(ThreadTest.class){
+			i++;
+		}
+	}
+	
+	/**
+	 * 对volatile变量的单个读/写，看成是使用同一个监视器锁对这些单个读/写操作做了同步
+	 */
+	@Test
+	public void valiteTest(){
+		//VolatileFeaturesExample example = new VolatileFeaturesExample();
+		int count = 20;
+		final CountDownLatch latch = new CountDownLatch(count);
+		for(int i=0; i<count; i++){
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					//example.getAndIncrement();
+						for(int j=0; j<100000; j++){
+							increase();
+						}
+						latch.countDown();
+					}
+				
+			}).start();
+		}
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(i);
+		//System.out.println(example.get());
 		
 	}
-	
-	public static class SleepInterrupt extends Object implements Runnable{  
-        public void run(){  
-            try{  
-                System.out.println("thread-sleep for 2000 seconds"); 
-                //睡眠也可中断
-                Thread.sleep(2000000);  
-                System.out.println("thread -waked up");  
-            }catch(InterruptedException e){  
-                System.out.println("thread-interrupted while sleeping");  
-                return;    
-            }  
-            System.out.println("thread-leaving normally");  
-        }  
-    }
-
-	@Test
-    public  void interrupte() {
-
-        SleepInterrupt si = new SleepInterrupt();  
-        Thread t = new Thread(si);  
-        t.start();  
-
-        //主线程休眠2秒，从而确保刚才启动的线程有机会执行一段时间  
-        try {  
-            Thread.sleep(2000);   
-        }catch(InterruptedException e){  
-            e.printStackTrace();  
-        }  
-        System.out.println("main() - interrupting other thread");  
-        //中断线程t  
-        t.interrupt();  
-
-        System.out.println("main() - leaving");  
-
-    }
 }
 
-class ReadThread extends Thread{
-	BoundedBuffer buffer;
-	String name;
-	
-	public ReadThread(String name,BoundedBuffer buffer){
-		this.buffer = buffer;
-		this.name = name;
-	}
-	
-	public void run(){
-		try {
-			buffer.take(name);
+
+class VolatileFeaturesExample {
+    long vl = 0L;               // 64位的long型普通变量
+
+    public synchronized void set(long l) {     //对单个的普通 变量的写用同一个监视器同步
+        vl = l;
+    }
+
+    public void getAndIncrement () { //普通方法调用
+        long temp = get();           //调用已同步的读方法
+        temp += 1L;                  //普通写操作
+        try {
+			Thread.sleep(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
+        set(temp);                   //调用已同步的写方法
+    }
+    public synchronized long get() { 
+    //对单个的普通变量的读用同一个监视器同步
+        return vl;
+    }
 }
-
-
-class WriteThread extends Thread{
-	BoundedBuffer buffer;
-	String name;
-	public WriteThread(String name,BoundedBuffer buffer){
-		this.buffer = buffer;
-		this.name = name;
-	}
-	
-	public void run(){
-		try {
-			buffer.put(name);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-}
-
-
-
-
-/**
- *  栈读写的实现
- * @author dasouche
- *
- */
-class BoundedBuffer {  
-	   final Lock lock = new ReentrantLock();//锁对象  
-	   final Condition notFull  = lock.newCondition();//写线程条件   
-	   final Condition notEmpty = lock.newCondition();//读线程条件   
-	  
-	   final Object[] items = new Object[20];//缓存队列  
-	   //int putptr/*写索引*/, takeptr/*读索引*/, 
-	   static int count/*队列中存在的数据个数*/;  
-	  
-	   public void put(String name) throws InterruptedException {  
-	     lock.lock(); //此时变量x保存下来
-	     System.out.println(name+ " 抢到锁");
-	     try {  
-	       while (count == items.length){//如果队列满了   
-	    	   System.out.println(name+ " 阻塞");
-	    	   notFull.await();//阻塞写线程,自动释放锁  唤醒之后且拿到锁将在此处继续往下执行 x为之前保存的
-	    	   System.out.println(name+ " 阻塞 唤醒");
-	       }
-	       if(count == items.length){
-	    	   return;
-	       }
-	       items[count++] = count-1;//赋值   
-	       notEmpty.signal();//唤醒读线程  
-	     } finally {  
-	       System.out.println(name+ " 写入 count= " + (count-1));
-	       System.out.println(name + " 释放锁");
-	       lock.unlock();  
-	     }  
-	   }  
-	  
-	   public Object take(String name) throws InterruptedException {  
-	     lock.lock();  
-	     System.out.println(name+ " 抢到锁");
-	     try {  
-	       while (count == 0){//如果队列为空  
-	    	   System.out.println(name+ " 阻塞");
-	    	   notEmpty.await();//阻塞读线程,自动释放锁  唤醒之后且拿到锁将在此处继续往下执行
-	    	   System.out.println(name+ " 阻塞 唤醒");
-	    	   
-	       }
-	       if (count == 0) {
-	    	   return null;
-	       }
-	       Object x = items[--count];//取值   
-	       notFull.signal();//唤醒写线程  
-	       System.out.println(name+ " 读出 count= " + (count) + " x= " + x);
-	       return x;  
-	     } finally {  
-	       System.out.println(name + " 释放锁");
-	       lock.unlock();  
-	     }  
-	   }   
-	 }  
