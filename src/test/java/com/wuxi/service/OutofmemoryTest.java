2697513482060e27cd406199c2dadec5f18ec6ad
zhanghui <zhanghui@souche.com>
2018-03-05 22:28:40 +0800

    update

--- src/test/java/com/wuxi/service/OutofmemoryTest.java
+++ src/test/java/com/wuxi/service/OutofmemoryTest.java
@@ -5,16 +5,21 @@ import java.util.List;
 
 public class OutofmemoryTest {
 
-	/**
-	 * -Xms20M -Xmx20M -Xmn10M  -XX:+PrintGC -XX:+PrintGCDetails
-		-XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError
-		-Xloggc:gc.log
-	 * @param args
-	 */
+	
+	
 	public static void main(String[] args) {
-		List<OutofmemoryTest> list = new ArrayList<OutofmemoryTest>();
-		while(true){
-			list.add(new OutofmemoryTest());
-		}
+		/**
+		 * -Xms20M -Xmx20M -Xmn10M  -XX:+PrintGC -XX:+PrintGCDetails
+			-XX:+PrintGCDateStamps -XX:+HeapDumpOnOutOfMemoryError
+			-Xloggc:gc.log
+		 * @param args
+		 */
+//		List<OutofmemoryTest> list = new ArrayList<OutofmemoryTest>();
+//		while(true){
+//			list.add(new OutofmemoryTest());
+//		}
+		
+		
 	}
+	
 }
